18a27dad21846522b926aa06545b86b9af045aff
Nick Ryder <nickryder@openai.com>
2021-03-31 19:38:41 -0700

    more error handling arround settings.yaml

--- PoEQuery/__init__.py
+++ PoEQuery/__init__.py
@@ -3,14 +3,22 @@ import os
 import yaml
 
 __settings_path__ = join(dirname(dirname(__file__)), "settings.yaml")
+__example_settings_path__ = join(dirname(dirname(__file__)), "example_settings.yaml")
 
 if not os.exists(__settings_path__):
     raise FileExistsError(
         "settings.yaml does not exist, please copy from example_settings.yaml and fill in necessary information"
     )
 
+if not os.exists(__example_settings_path__):
+    raise FileExistsError("example_settings.yaml does not exist")
+
 with open(join(dirname(dirname(__file__)), "settings.yaml"), "r") as f:
-    settings = yaml.load(f, Loader=yaml.FullLoader)
+    settings = yaml.load(f, Loader=yaml.SafeLoader)
+    example_settings = yaml.load(f, Loader=yaml.SafeLoader)
+    assert (
+        settings.keys() == example_settings.keys()
+    ), "please copy example_settings.yaml to settings.yaml"
 
 try:
     account_name = settings["account_name"]
