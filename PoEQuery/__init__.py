712bb943bb140708e817a2d62e0908f75d852655
Nick Ryder <nickryder@openai.com>
2021-03-31 19:35:53 -0700

    added in error message to instruct to copy from example_settings.yaml

--- PoEQuery/__init__.py
+++ PoEQuery/__init__.py
@@ -1,12 +1,24 @@
 from os.path import dirname, join
+import os
 import yaml
 
+__settings_path__ = join(dirname(dirname(__file__)), "settings.yaml")
+
+if not os.exists(__settings_path__):
+    raise FileExistsError(
+        "settings.yaml does not exist, please copy from example_settings.yaml and fill in necessary information"
+    )
 
 with open(join(dirname(dirname(__file__)), "settings.yaml"), "r") as f:
     settings = yaml.load(f, Loader=yaml.FullLoader)
 
-account_name = settings["account_name"]
-realm = settings["realm"]
-league_id = settings["league_id"]
-poesessid = settings["poesessid"]
-user_agent = settings["user_agent"]
+try:
+    account_name = settings["account_name"]
+    realm = settings["realm"]
+    league_id = settings["league_id"]
+    poesessid = settings["poesessid"]
+    user_agent = settings["user_agent"]
+except KeyError:
+    raise KeyError(
+        "please fill out all keys in settings.yaml, following template from examples.yaml"
+    )
