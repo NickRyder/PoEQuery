2d4286ac12264a08ca7070678baac44d77c94bba
Nick Ryder <nickryder@openai.com>
2021-04-04 21:38:16 -0700

    test to confirm .json() serialization works

--- PoEQuery/test_official_api_async.py
+++ PoEQuery/test_official_api_async.py
@@ -1,72 +1,49 @@
+from PoEQuery.official_api_query import OfficialApiQuery
 from PoEQuery.official_api_async import (
     characters,
-    fetch,
-    fetch_batched,
     items,
     passive_tree,
     search,
 )
+from PoEQuery.batched_fetch import fetch_batched
+
 import asyncio
-from tqdm import tqdm
 import pytest
 
 item_ids = [
-    "229ec3ed6037b122ef54d922fdc9c0ae8eaab590da5dc29fec9139803b2da442",
-    "9801a885b860bc01c5481185a73de06b4faecbd1966b69c3695b858128df4ae2",
-    "9821eb040c5d6ad5a1849dc47f63d0ef4d6acbce191390142d4fce6ce677d85f",
-    "3c09140f706df93ce4114cb9d1177be459567d5d85b1ae508053271aeef9d114",
-    "d66d2843e490b7b51699487d74f984015a833df8bbb0b25d062ede926b439d2b",
-    "2cb36a3f83d1049e843a495ba5b9ee62cb037fe7bb578815cb6b8e8d2248ff5e",
-    "9253d0a427243d42e0b7d9de7f470cafc913372ef3c685bb0812500eccd7461b",
-    "e3b33f355aff873996df52d388754a08f81a7e3cf94ebded5b84baef63cd696f",
-    "229ec3ed6037b122ef54d922fdc9c0ae8eaab590da5dc29fec9139803b2da442",
-    "9801a885b860bc01c5481185a73de06b4faecbd1966b69c3695b858128df4ae2",
-    "9821eb040c5d6ad5a1849dc47f63d0ef4d6acbce191390142d4fce6ce677d85f",
-    "3c09140f706df93ce4114cb9d1177be459567d5d85b1ae508053271aeef9d114",
-    "d66d2843e490b7b51699487d74f984015a833df8bbb0b25d062ede926b439d2b",
-    "2cb36a3f83d1049e843a495ba5b9ee62cb037fe7bb578815cb6b8e8d2248ff5e",
-    "9253d0a427243d42e0b7d9de7f470cafc913372ef3c685bb0812500eccd7461b",
-    "e3b33f355aff873996df52d388754a08f81a7e3cf94ebded5b84baef63cd696f",
+    "388785b8862730a8f112bfa62e6e207d44d39c7585de5cc7f81f18c21d5498e9",
+    "2d0d4561e3074cfe7e1b7a836108248bab54902b1c8f3fc04e407d8e4b679703",
+    "269a8332f4af43ee2c244486d9e5f84d7b0e0319a758ca86e5f8cca5250fbc7c",
+    "859de7bbf3724b20b5ed1a07d15c1ef29bdad613b0d4904e5857d199e898bcab",
+    "d3be0b726851c592597a0d188d75e258e41a54796c77fa9cb02d70b69cabdd30",
+    "43e3ebf23e01d98d3ffc1622a470359f6300ff32c78d9174475e213768a8cbc3",
+    "9db224474ff9183e70c1ddefa4e8c0bd6a0fa6e4cf053bb83f9017d1642367ed",
+    "26ecb874399777d4a28e314b0773f87205997ea60bf0c49cf73b1e65b0661226",
+    "8293b6469e1f2f983def6835f179264d45752bd538113ce3612a3f6e90c732a1",
+    "ac26f737f496ea4140ebefbb727bd3466530afa704b65085a07f4fa5d78c28a1",
 ]
 
 
-async def _test_search():
-    print("search")
-    return await search(
-        {
-            "query": {
-                "status": {"option": "online"},
-                "stats": [{"type": "and", "filters": []}],
-            },
-            "sort": {"price": "asc"},
-        },
-    )
-
-
-async def _test_fetch():
-    print("fetch")
-    return await fetch(item_ids[:10])
+query = OfficialApiQuery()
 
 
 def test_endpoints():
-    async def test_fetch_batched():
-        print("fetch_batched")
-        return await fetch_batched(item_ids)
+    asyncio.run(search(query))
+    asyncio.run(fetch_batched(item_ids))
 
-    asyncio.run(_test_search())
-    asyncio.run(_test_fetch())
-    asyncio.run(test_fetch_batched())
-
-    asyncio.run(characters("havoc616"))
-    passive_tree("havoc616", "Havoc_TwitchPrime")
-    asyncio.run(items("havoc616", "Havoc_TwitchPrime"))
+    asyncio.run(characters("Havoc6"))
+    passive_tree("Havoc6", "Havoc_TwitchPrime")
+    asyncio.run(items("Havoc6", "Havoc_TwitchPrime"))
 
 
-def test_async_gather():
+@pytest.mark.parametrize("use_cached", [True, False])
+def test_async_gather(use_cached):
     N = 10
 
     async def main():
-        things = [_test_fetch() for _ in range(N)] + [_test_search() for _ in range(N)]
+        things = [fetch_batched(item_ids, use_cached=use_cached) for _ in range(N)] + [
+            search(query, use_cached=use_cached) for _ in range(N)
+        ]
         return await asyncio.gather(*things)
 
     asyncio.run(main())
@@ -74,20 +51,16 @@ def test_async_gather():
 
 def test_multiple_fetch_batched():
 
-    N = 100
-    _tqdm = tqdm(total=N)
+    N = 30
 
-    async def sleep_fetch(item_ids, sleep_time, tqdm: tqdm = None):
+    async def sleep_fetch(item_ids, sleep_time):
         await asyncio.sleep(sleep_time)
         results = await fetch_batched(item_ids)
-        if tqdm is not None:
-            tqdm.update(len(results))
 
     coroutines = [
         sleep_fetch(
             ["229ec3ed6037b122ef54d922fdc9c0ae8eaab590da5dc29fec9139803b2da442"],
             0.1 * idx,
-            _tqdm,
         )
         for idx in range(N)
     ]
@@ -99,4 +72,4 @@ def test_multiple_fetch_batched():
 
 
 if __name__ == "__main__":
-    pytest.main()
+    pytest.main(["-vxs"])
