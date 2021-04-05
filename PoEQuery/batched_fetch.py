6927f85e2c2ae89a7d2e295b8088238fa48948d0
Nick Ryder <nickryder@openai.com>
2021-04-04 21:33:22 -0700

    added in caching and made fetch_batched default

---
+++ PoEQuery/batched_fetch.py
@@ -0,0 +1,125 @@
+"""
+To maximize our fetches per second, we want to batch as many fetches as we can.
+To accomplish this we create a global queue of fetches `to_fetch_queue`
+Everytime we submit a list of item_ids, it gets added to the global queue as asyncio.Futures
+
+While not all our futures are done, we send `_fetch_batched` which is wrapped with a rate limit respecter
+once done awaiting the rate limit, the inner function is executed, which pops of 10 items from the queue
+it then sets the results of the Futures it pops off with the results from the request.get
+
+In fetch_batched, after creating the futures, and awaiting `_fetch_batched`, we
+await the results of the futures, and then return them.
+"""
+
+import asyncio
+import logging
+from asyncio import Lock
+from asyncio.events import AbstractEventLoop
+from asyncio.futures import Future
+from collections import defaultdict, deque
+from typing import Dict, List
+
+import time
+
+import requests
+
+from PoEQuery import __diskcache_path__, poesessid, user_agent
+from PoEQuery.x_rate_limiter import rate_limited
+
+from PoEQuery.cache import cache_results
+
+USER_AGENT_HEADER = {"user-agent": user_agent}
+
+API_FETCH = "https://www.pathofexile.com/api/trade/fetch"
+
+
+class ItemFuture(Future):
+    item_id: str
+
+    def __init__(self, item_id):
+        self.item_id = item_id
+        super().__init__()
+
+
+# TODO: wrap these in a class?
+to_fetch_queue: deque = deque()
+to_fetch_id_to_future: Dict[str, ItemFuture] = dict()
+fetch_queue_lock: Dict[AbstractEventLoop, Lock] = defaultdict(Lock)
+
+
+async def fetch_batched(item_ids: List[str], use_cached=True) -> List[dict]:
+    return await asyncio.gather(
+        *[queue_single_fetch(item_id, use_cached=use_cached) for item_id in item_ids]
+    )
+
+
+@cache_results("fetch_results", key=lambda item_id: item_id)
+async def queue_single_fetch(
+    item_id: str,
+) -> List[dict]:
+    if item_id not in to_fetch_id_to_future:
+        item_future: ItemFuture = ItemFuture(item_id)
+        to_fetch_queue.append(item_future)
+        to_fetch_id_to_future[item_id] = item_future
+    else:
+        logging.info(f"Item already in queue to be fetched: {item_id}")
+        item_future = to_fetch_id_to_future[item_id]
+
+    async with fetch_queue_lock[asyncio.get_running_loop()]:
+        while not item_future.done():
+            await _fetch_batched()
+
+    returned_item = await item_future
+    if returned_item is None:
+        logging.warning(f"Received null result from fetch: {item_future.item_id}")
+    else:
+        assert returned_item["id"] == item_future.item_id
+    return returned_item
+
+
+@rate_limited("trade-fetch-request-limit")
+def _fetch_batched() -> requests.Response:
+    assert len(to_fetch_queue) > 0, "queue should be populated"
+    item_futures = []
+    for _ in range(10):
+        try:
+            item_future = to_fetch_queue.popleft()
+            item_futures.append(item_future)
+            del to_fetch_id_to_future[item_future.item_id]
+        except IndexError:
+            pass
+
+    item_ids = [item_future.item_id for item_future in item_futures]
+    response = requests.get(
+        f"{API_FETCH}/{','.join(item_ids)}",
+        headers=USER_AGENT_HEADER,
+        cookies=dict(POESESSID=poesessid),
+    )
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        logging.warning(e)
+
+    for item_result, item_future in zip(response.json()["result"], item_futures):
+        item_future.set_result(item_result)
+    return response
+
+
+if __name__ == "__main__":
+    item_ids = [
+        "388785b8862730a8f112bfa62e6e207d44d39c7585de5cc7f81f18c21d5498e9",
+        "2d0d4561e3074cfe7e1b7a836108248bab54902b1c8f3fc04e407d8e4b679703",
+        "269a8332f4af43ee2c244486d9e5f84d7b0e0319a758ca86e5f8cca5250fbc7c",
+        "859de7bbf3724b20b5ed1a07d15c1ef29bdad613b0d4904e5857d199e898bcab",
+        "d3be0b726851c592597a0d188d75e258e41a54796c77fa9cb02d70b69cabdd30",
+        "43e3ebf23e01d98d3ffc1622a470359f6300ff32c78d9174475e213768a8cbc3",
+        "9db224474ff9183e70c1ddefa4e8c0bd6a0fa6e4cf053bb83f9017d1642367ed",
+        "26ecb874399777d4a28e314b0773f87205997ea60bf0c49cf73b1e65b0661226",
+        "8293b6469e1f2f983def6835f179264d45752bd538113ce3612a3f6e90c732a1",
+        "ac26f737f496ea4140ebefbb727bd3466530afa704b65085a07f4fa5d78c28a1",
+    ]
+
+    for _ in range(10):
+        asyncio.run(fetch_batched(item_ids * 2))
+        time.sleep(1)
--- PoEQuery/official_api_async.py
+++ PoEQuery/official_api_async.py
@@ -1,15 +1,13 @@
-import asyncio
-import logging
-from asyncio import Lock
-from asyncio.events import AbstractEventLoop
-from asyncio.futures import Future
-from collections import defaultdict, deque
-from typing import Dict, List
+from PoEQuery.official_api_query import OfficialApiQuery
+from PoEQuery.batched_fetch import fetch_batched
+from typing import List
 
 import requests
 
 from PoEQuery import league_id, poesessid, realm, user_agent
 from PoEQuery.x_rate_limiter import rate_limited
+
+from PoEQuery.cache import cache_results
 
 USER_AGENT_HEADER = {"user-agent": user_agent}
 
@@ -26,9 +24,10 @@ API_PASSIVE = "https://www.pathofexile.com/character-window/get-passive-skills"
 API_ITEM = "https://www.pathofexile.com/character-window/get-items"
 
 
+@cache_results("search_results", key=lambda query: query.json())
 @rate_limited("trade-search-request-limit")
-def search(query: dict = None) -> requests.Response:
-    query = {} if query is None else query
+def search(query: OfficialApiQuery = None) -> requests.Response:
+    query = {} if query is None else query.json()
     return requests.post(
         f"{API_SEARCH}/{league_id}",
         headers=USER_AGENT_HEADER,
@@ -37,13 +36,9 @@ def search(query: dict = None) -> requests.Response:
     )
 
 
-@rate_limited("trade-fetch-request-limit")
+# See PoEQuery.batched_fetch for a batched, cached variant
 def fetch(item_ids: List[str]) -> requests.Response:
-    return requests.get(
-        f"{API_FETCH}/{','.join(item_ids)}",
-        headers=USER_AGENT_HEADER,
-        cookies=dict(POESESSID=poesessid),
-    )
+    return fetch_batched(item_ids)
 
 
 @rate_limited("backend-item-request-limit")
@@ -84,77 +79,3 @@ def items(
 ) -> requests.Response:
     params = {"accountName": account_name, "character": character_name, "realm": realm}
     return requests.get(API_ITEM, params=params, headers=USER_AGENT_HEADER)
-
-
-###
-# Batched Fetching
-###
-
-"""
-To maximize our fetches per second, we want to batch as many fetches as we can.
-To accomplish this we create a global queue of fetches `to_fetch_queue`
-Everytime we submit a list of item_ids, it gets added to the global queue as asyncio.Futures
-
-While not all our futures are done, we send `_fetch_batched` which is wrapped with a rate limit respecter
-once done awaiting the rate limit, the inner function is executed, which pops of 10 items from the queue
-it then sets the results of the Futures it pops off with the results from the request.get
-
-In fetch_batched, after creating the futures, and awaiting `_fetch_batched`, we
-await the results of the futures, and then return them.
-"""
-
-to_fetch_queue: deque = deque()
-fetch_queue_lock: Dict[AbstractEventLoop, Lock] = defaultdict(Lock)
-
-
-class ItemFuture(Future):
-    item_id: str
-
-    def __init__(self, item_id):
-        self.item_id = item_id
-        super().__init__()
-
-
-async def fetch_batched(item_ids: List[str]) -> List[dict]:
-    item_futures: List[ItemFuture] = []
-    for item_id in item_ids:
-        item_future: ItemFuture = ItemFuture(item_id)
-        item_futures.append(item_future)
-        to_fetch_queue.append(item_future)
-
-    async with fetch_queue_lock[asyncio.get_running_loop()]:
-        all_done = all([item_future.done() for item_future in item_futures])
-        while not all_done:
-            await _fetch_batched()
-            all_done = all([item_future.done() for item_future in item_futures])
-
-    returned_results = []
-    for item_future in item_futures:
-        returned_item = await item_future
-        if returned_item is None:
-            logging.warning(f"received null result from fetch: {item_future.item_id}")
-        else:
-            assert returned_item["id"] == item_future.item_id
-        returned_results.append(returned_item)
-    return returned_results
-
-
-@rate_limited("trade-fetch-request-limit")
-def _fetch_batched() -> requests.Response:
-    assert len(to_fetch_queue) > 0, "queue should be populated"
-    item_futures = []
-    for _ in range(10):
-        try:
-            item_futures.append(to_fetch_queue.popleft())
-        except IndexError:
-            pass
-
-    item_ids = [item_future.item_id for item_future in item_futures]
-    result = requests.get(
-        f"{API_FETCH}/{','.join(item_ids)}",
-        headers=USER_AGENT_HEADER,
-        cookies=dict(POESESSID=poesessid),
-    )
-    for item_result, item_future in zip(result.json()["result"], item_futures):
-        item_future.set_result(item_result)
-    return result
