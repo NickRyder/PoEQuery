0b33d0c9ae7110e72b898db0d9e3dc67b1514a46
Nick Ryder <nickryder@openai.com>
2021-04-28 20:58:10 -0700

    added in coroutines to cache

--- PoEQuery/cache.py
+++ PoEQuery/cache.py
@@ -3,6 +3,7 @@ import time
 from PoEQuery import __diskcache_path__
 import os
 import logging
+import inspect
 from functools import lru_cache
 
 DEFAULT_EXPIRE = 60 * 60 * 24 * 7 * 2
@@ -34,25 +35,42 @@ def cache_results(subdirectory, key, expire=DEFAULT_EXPIRE):
     directory = os.path.join(__diskcache_path__, subdirectory)
 
     def cache_results_decorator(async_fn):
-        async def cached_fn(
-            *args,
-            use_cached=True,
-            **kwargs,
-        ):
-            cache = _lazy_load_cache(directory)
-            _key = key(*args, **kwargs)
-            if _key in cache and use_cached:
-                cached_result, expire_time = cache.get(_key, expire_time=True)
-                # logging.info(
-                #     f"Found cached result, expires in {expire_time - time.time():0.2f}s "
-                #     f"({(expire_time - time.time())/(60*60*24):0.2f} days)"
-                # )
-                return cached_result
-            else:
-                result = await async_fn(*args, **kwargs)
-                cache.set(_key, result, expire=expire)
-                return result
 
-        return cached_fn
+        if inspect.iscoroutinefunction(async_fn):
+
+            async def async_cached_fn(
+                *args,
+                use_cached=True,
+                **kwargs,
+            ):
+                cache = _lazy_load_cache(directory)
+                _key = key(*args, **kwargs)
+                if _key in cache and use_cached:
+                    cached_result, expire_time = cache.get(_key, expire_time=True)
+                    return cached_result
+                else:
+                    result = await async_fn(*args, **kwargs)
+                    cache.set(_key, result, expire=expire)
+                    return result
+
+            return async_cached_fn
+        else:
+
+            def cached_fn(
+                *args,
+                use_cached=True,
+                **kwargs,
+            ):
+                cache = _lazy_load_cache(directory)
+                _key = key(*args, **kwargs)
+                if _key in cache and use_cached:
+                    cached_result, expire_time = cache.get(_key, expire_time=True)
+                    return cached_result
+                else:
+                    result = async_fn(*args, **kwargs)
+                    cache.set(_key, result, expire=expire)
+                    return result
+
+            return cached_fn
 
     return cache_results_decorator
