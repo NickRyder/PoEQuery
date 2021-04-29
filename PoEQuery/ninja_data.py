970eab1194ceb5fdd081949742aba0d8e891ee3a
Nick Ryder <nickryder@openai.com>
2021-04-28 21:04:40 -0700

    added cache and leagues

--- PoEQuery/ninja_data.py
+++ PoEQuery/ninja_data.py
@@ -1,3 +1,4 @@
+from PoEQuery.cache import cache_results
 from collections import defaultdict
 from requests_html import HTMLSession
 from functools import lru_cache
@@ -7,6 +8,44 @@ from io import BytesIO, StringIO
 import zipfile
 
 
+CURRENT_LEAGUES = [
+    "Harvest",
+    "HC Harvest",
+    "Delirium",
+    "HC Delirium",
+    "Metamorph",
+    "HC Metamorph",
+    "Blight",
+    "HC Blight",
+    "Legion",
+    "HC Legion",
+    "Synthesis",
+    "HC Synthesis",
+    "Betrayal",
+    "HC Betrayal",
+    "Delve",
+    "HC Delve",
+    "Incursion Flashback",
+    "Incursion HC Flashback",
+    "Incursion",
+    "HC Incursion",
+    "Flashback",
+    "HC Flashback",
+    "Bestiary",
+    "HC Bestiary",
+    "Abyss",
+    "HC Abyss",
+    "Harbinger",
+    "HC Harbinger",
+    "Legacy",
+    "HC Legacy",
+    "Breach",
+    "HC Breach",
+    "Essence",
+    "HC Essence",
+]
+
+
 @lru_cache()
 def scrape_data_links():
     with HTMLSession() as session:
@@ -14,14 +53,14 @@ def scrape_data_links():
         # ninja uses js
         r.html.render()
 
-        league_to_link = defaultdict(lambda: defaultdict())
+        league_to_link = {}
         for challenge_league in r.html.find("div.challenge-leagues"):
             for link_element in challenge_league.find("a"):
                 league_to_link[link_element.text] = link_element.attrs["href"]
         return league_to_link
 
 
-@lru_cache()
+@cache_results("ninja_data", key=lambda league_name: league_name)
 def get_past_league_data(league_name):
     data_links = scrape_data_links()
     try:
@@ -39,7 +78,7 @@ def get_past_league_data(league_name):
             zip = zipfile.ZipFile(BytesIO(response.content))
             zipped_contents = {}
             for info in zip.infolist():
-                zipped_contents[info.filename] = pd.read_csv(
+                zipped_contents[info.filename.split(".")[-2]] = pd.read_csv(
                     StringIO(zip.read(info.filename).decode()), sep=";"
                 )
             return zipped_contents
